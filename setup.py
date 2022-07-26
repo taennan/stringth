0d093c4119fa7aa4b8884d199fee9859255129de
taennan <taennan@zohomail.com>
2022-07-26 12:37:15 +1200

    bumped pathc version number to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name="stringth",
-    version="0.0.1",
+    version="0.0.2",
     license="Apache-2.0",
     author="taennan",
     author_email="taennan@scoobyapps.com",
